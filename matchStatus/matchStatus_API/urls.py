d566e8ee1dbb938af49a5de8ed9974d97c6d52d1
Victor <vmvg29@gmail.com>
2022-12-22 19:15:55 +0100

    Swagger y Docker fixed

--- matchStatus/matchStatus_API/urls.py
+++ matchStatus/matchStatus_API/urls.py
@@ -22,6 +22,10 @@ router.register(r'match-status', MatchStatusViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   #  re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   #  re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
