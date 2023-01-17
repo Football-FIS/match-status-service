53680069e2281fc001c66d7faaa8b0739358fb0f
Diego Monsalves <61249631+diemonvaz@users.noreply.github.com>
2023-01-17 13:57:21 +0100

    Cambio en allowed hosts

--- matchStatus/matchStatus/settings.py
+++ matchStatus/matchStatus/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8*23gd&8$quu(fy6j4$aoiidfajfaklbgayowe!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
